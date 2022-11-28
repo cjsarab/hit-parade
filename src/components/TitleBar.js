0e9da8df915529f7b80f1384fe5b0cd587b9b067
Cammy Smith <cjsarab1991@gmail.com>
2022-11-28 20:51:00 +0000

    please work now

--- src/components/TitleBar.js
+++ src/components/TitleBar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const TitleBar = ({handleSelectChange, genres}) =>{
   return (
     <div>
-      <h1>Changed to Camerino's CodeClan Hit Parade</h1>
+      <h1>Changed to Cammy's CodeClan Hit Parade</h1>
       <select onChange={handleSelectChange}>
         {genres.map(genre => {
           return <option key={genre.name} value={genre.url}>{genre.name}</option>
